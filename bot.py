67434f433ed019f2039642f99e124a2115367d94
MorganFisher2007 <morgantf2000@gmail.com>
2022-02-22 09:45:10 -0800

    VERSION 1!

--- bot.py
+++ bot.py
@@ -1,16 +1,14 @@
 import re
-import sys
 import json
-import time
-import random
+from hashlib import new
 from selenium import webdriver
 from stockfish import Stockfish
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-enable_automation = "-a" in sys.argv
-
-stockfish = Stockfish(parameters={"Threads": 8})
-stockfish.set_depth(16)
+stockfish = Stockfish('/Users/morgan/Desktop/Stockfish/src/stockfish', parameters={"Threads": 8})
+stockfish.set_depth(10)
 
 prevFen = ""
 
@@ -18,8 +16,9 @@ chrm_caps = webdriver.DesiredCapabilities.CHROME.copy()
 chrm_caps['goog:loggingPrefs'] = { 'performance':'ALL' }
 
 driver = webdriver.Chrome(desired_capabilities=chrm_caps)
-action = ActionChains(driver)
+actions = ActionChains(driver)
 driver.get("https://lichess.org")
+
 
 def WebSocketLog():
     payloadCache = []
@@ -29,27 +28,51 @@ def WebSocketLog():
             payloadCache.append(wsJson["message"]["params"]["response"]["payloadData"])
     return payloadCache
 
+def drawArrow(board, move):
+    corr = 1
+    if 'orientation-black' in driver.page_source:
+        corr = -1
+    coords = 'x1="c1" y1="c2" x2="c3" y2="c4"'
+    coords = coords.replace("c1", str(corr*((ord(move[0]) & 31)-4.5)))
+    coords = coords.replace("c2", str(corr*(4.5 - float(move[1]))))
+    coords = coords.replace("c3", str(corr*((ord(move[2]) & 31)-4.5)))
+    coords = coords.replace("c4", str(corr*(4.5 - float(move[3]))))
+    arrow = '<line stroke="#15781B" stroke-width="0.15625" stroke-linecap="round" marker-end="url(#arrowhead-g)" opacity="1" cgHash="288,288,green"' + coords + '></line>'
+    script = "var ele=arguments[0]; ele.innerHTML = '" + arrow + "';"
+    driver.execute_script(script, board)
+
+
 while True:
     if '<button class="fbt resign" title="Resign"><span' in driver.page_source:
+        g = driver.find_element(By.TAG_NAME ,"g")
         for i in reversed(WebSocketLog()):
             newFen = re.findall(r'fen":"(.*?)"', i)
             if len(newFen) != 0 and prevFen != newFen[0]:
                 prevFen = newFen[0]
                 if 'Your turn - Play' in driver.page_source:
-                     if 'white manipulable' in driver.page_source:
-                         stockfish.set_fen_position(str(newFen)[1:-1]+' w')
-                         print(stockfish.get_top_moves())
-                         move = stockfish.get_top_moves()[0]["Move"]
-                         if enable_automation:
-                             driver.execute_script('lichess.socket.send("move",{"u":"' + move + '","s":' + str(random.randint(3, 10)) + '})')
-                         print(move)
-                         break
-                     elif 'black manipulable' in driver.page_source:
-                         stockfish.set_fen_position(str(newFen)[1:-1]+' b')
-                         print(stockfish.get_top_moves())
-                         move = stockfish.get_top_moves()[0]["Move"]
-                         if enable_automation:
-                             driver.execute_script('lichess.socket.send("move",{"u":"' + move + '","s": ' + str(random.randint(3, 10)) +'})')
-                         print(move)
-                         break
-                     break 
+                    moves = []
+                    if 'white manipulable' in driver.page_source:
+                        stockfish.set_fen_position(str(newFen)[1:-1]+' w')
+                        '''for i in stockfish.get_top_moves():
+                            moves.append(i['Move'])
+                            if i['Centipawn'] != None:
+                                moves.append(i['Centipawn'])
+                            else:
+                                moves.append('m' + moves['Mate'])
+                        for i in moves:
+
+                        print(moves)'''
+                        drawArrow(g, stockfish.get_best_move())
+                        break
+                    elif 'black manipulable' in driver.page_source:
+                        stockfish.set_fen_position(str(newFen)[1:-1]+' b')
+                        '''for i in stockfish.get_top_moves():
+                            moves.append(i['Move'])
+                            if i['Centipawn'] != None:
+                                moves.append(i['Centipawn'])
+                            else:
+                                moves.append('m' + moves['Mate'])
+                        print(moves)'''
+                        drawArrow(g, stockfish.get_best_move())
+                        break
+                    break
